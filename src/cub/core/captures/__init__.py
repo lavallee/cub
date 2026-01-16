e3a417db33ef4ee45f23483627c702cd2d5ee6b5
Marc Lavallee <mlavallee@gmail.com>
2026-01-16 20:09:52 +0000

    bd sync: 2026-01-16 20:09:52

--- src/cub/core/captures/__init__.py
+++ src/cub/core/captures/__init__.py
@@ -7,11 +7,9 @@ material for the vision-to-tasks pipeline.
 """
 
 from cub.core.captures.models import Capture, CaptureSource, CaptureStatus
-from cub.core.captures.store import CaptureStore
 
 __all__ = [
     "Capture",
     "CaptureSource",
     "CaptureStatus",
-    "CaptureStore",
 ]
